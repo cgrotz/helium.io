b5492f8fc87ea6b8e84f1a0bfdd38ce4f1e2c229
zerooneinfinity <christophgrotz@gmail.com>
2013-05-05 23:28:57 +0200

    Cleanup

--- roadrunner-tomcat/src/main/java/org/roadrunner/server/RoadrunnerMessageInbound.java
+++ roadrunner-tomcat/src/main/java/org/roadrunner/server/RoadrunnerMessageInbound.java
@@ -12,7 +12,6 @@ import javax.jcr.LoginException;
 import javax.jcr.RepositoryException;
 
 import org.apache.catalina.websocket.MessageInbound;
-import org.apache.catalina.websocket.WsOutbound;
 import org.infinispan.schematic.document.ParsingException;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -43,23 +42,142 @@ public class RoadrunnerMessageInbound extends MessageInbound implements
 	private Set<String> attached_listeners = Sets.newHashSet();
 
 	public RoadrunnerMessageInbound(String servletPath, String path,
-			DataServiceFactory dataServiceFactory,AuthenticationServiceFactory authenticationServiceFactory) throws ParsingException,
-			ConfigurationException, LoginException, RepositoryException,
-			FileNotFoundException, DataServiceCreationException {
+			DataServiceFactory dataServiceFactory,
+			AuthenticationServiceFactory authenticationServiceFactory)
+			throws ParsingException, ConfigurationException, LoginException,
+			RepositoryException, FileNotFoundException,
+			DataServiceCreationException {
 		this.servletPath = servletPath;
 
 		this.repositoryName = path.indexOf("/") > -1 ? path.substring(0,
 				path.indexOf("/")) : path;
 
 		connections.add(this);
-		authorizationService = authenticationServiceFactory.getAuthorizationService(repositoryName);
-		dataService = dataServiceFactory.getDataService(authorizationService, repositoryName);
+		authorizationService = authenticationServiceFactory
+				.getAuthorizationService(repositoryName);
+		dataService = dataServiceFactory.getDataService(authorizationService,
+				repositoryName);
 		dataService.addListener(this);
+	}
+
+	private void broadcast(String message) throws IOException {
+		for (RoadrunnerMessageInbound connection : connections) {
+			CharBuffer buffer = CharBuffer.wrap(message);
+			connection.getWsOutbound().writeTextMessage(buffer);
+		}
+	}
+
+	@Override
+	public void child_added(String name, String path, String parent,
+			JSONObject node, String prevChildName, boolean hasChildren,
+			long numChildren) {
+		try {
+			JSONObject broadcast = new JSONObject();
+			broadcast.put("type", "child_added");
+			broadcast.put("name", name);
+			broadcast.put("path", path);
+			broadcast.put("parent", parent);
+			broadcast.put("payload", node);
+			broadcast.put("prevChildName", prevChildName);
+			broadcast.put("hasChildren", hasChildren);
+			broadcast.put("numChildren", numChildren);
+			if (listenerAttached(path)) {
+				getWsOutbound().writeTextMessage(
+						CharBuffer.wrap(broadcast.toString()));
+			}
+		} catch (Exception exp) {
+			throw new RuntimeException(exp);
+		}
+	}
+
+	@Override
+	public void child_changed(String name, String path, String parent,
+			JSONObject node, String prevChildName, boolean hasChildren,
+			long numChildren) {
+		try {
+			JSONObject broadcast = new JSONObject();
+			broadcast.put("type", "child_changed");
+			broadcast.put("name", name);
+			broadcast.put("path", path);
+			broadcast.put("parent", parent);
+			broadcast.put("payload", node);
+			broadcast.put("prevChildName", prevChildName);
+			broadcast.put("hasChildren", hasChildren);
+			broadcast.put("numChildren", numChildren);
+			getWsOutbound().writeTextMessage(
+					CharBuffer.wrap(broadcast.toString()));
+		} catch (Exception exp) {
+			throw new RuntimeException(exp);
+		}
+	}
+
+	@Override
+	public void child_moved(JSONObject childSnapshot, String prevChildName,
+			boolean hasChildren, long numChildren) {
+		try {
+			JSONObject broadcast = new JSONObject();
+			broadcast.put("type", "child_moved");
+			broadcast.put("payload", childSnapshot);
+			broadcast.put("prevChildName", prevChildName);
+			broadcast.put("hasChildren", hasChildren);
+			broadcast.put("numChildren", numChildren);
+			getWsOutbound().writeTextMessage(
+					CharBuffer.wrap(broadcast.toString()));
+		} catch (Exception exp) {
+			throw new RuntimeException(exp);
+		}
+	}
+
+	@Override
+	public void child_removed(String path, JSONObject payload) {
+		try {
+			JSONObject broadcast = new JSONObject();
+			broadcast.put("type", "child_removed");
+			broadcast.put("path", path);
+			broadcast.put("payload", payload);
+			getWsOutbound().writeTextMessage(
+					CharBuffer.wrap(broadcast.toString()));
+		} catch (Exception exp) {
+			throw new RuntimeException(exp);
+		}
+	}
+
+	private String extractPath(JSONObject message) throws JSONException {
+		int servletPathLength = servletPath.length();
+		int repositoryNameLength = repositoryName.length();
+		String path = (String) message.get("path");
+		int indexOfServletPath = path.indexOf(servletPath);
+		if (indexOfServletPath > -1) {
+			int substringIndex = indexOfServletPath + servletPathLength
+					+ repositoryNameLength + 1;
+			return path.substring(substringIndex);
+		} else {
+			return path;
+		}
+	}
+
+	private boolean listenerAttached(String path) {
+		for (String listenerPath : attached_listeners) {
+			if (path.startsWith(listenerPath)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
 	protected void onBinaryMessage(ByteBuffer message) throws IOException {
 		throw new UnsupportedOperationException("Binary message not supported.");
+	}
+
+	@Override
+	protected void onClose(int status) {
+		super.onClose(status);
+		try {
+			dataService.shutdown();
+		} catch (Exception exp) {
+			throw new RuntimeException(exp);
+		}
 	}
 
 	@Override()
@@ -68,15 +186,13 @@ public class RoadrunnerMessageInbound extends MessageInbound implements
 			JSONObject message = new JSONObject(msg.toString());
 			String messageType = (String) message.get("type");
 			String path = extractPath(message);
-			
+
 			if ("attached_listener".equalsIgnoreCase(messageType)) {
 				attached_listeners.add(path);
 				dataService.sync(path);
-			}
-			else if ("detached_listener".equalsIgnoreCase(messageType)) {
+			} else if ("detached_listener".equalsIgnoreCase(messageType)) {
 				attached_listeners.remove(path);
-			}
-			else if ("push".equalsIgnoreCase(messageType)) {
+			} else if ("push".equalsIgnoreCase(messageType)) {
 				JSONObject payload;
 				if (message.has("payload")) {
 					payload = (JSONObject) message.get("payload");
@@ -99,7 +215,7 @@ public class RoadrunnerMessageInbound extends MessageInbound implements
 					JSONObject broadcast = new JSONObject();
 					broadcast.put("type", "pushed");
 					broadcast.put("name", nodeName);
-					broadcast.put("path", path+"/"+nodeName);
+					broadcast.put("path", path + "/" + nodeName);
 					broadcast.put("parent", pushed.getParent());
 					broadcast.put("payload", pushed.getPayload());
 					broadcast.put("prevChildName", pushed.getPrevChildName());
@@ -112,155 +228,33 @@ public class RoadrunnerMessageInbound extends MessageInbound implements
 				JSONObject payload;
 				if (message.has("payload")) {
 					Object obj = message.get("payload");
-					if(obj instanceof JSONObject)
-					{
+					if (obj instanceof JSONObject) {
 						payload = (JSONObject) obj;
 						if (payload instanceof JSONObject) {
 							if (Strings.isNullOrEmpty(path)) {
-								dataService.update(null, (JSONObject) payload);
+								dataService.update(null, payload);
 							} else {
-								dataService.update(path, (JSONObject) payload);
+								dataService.update(path, payload);
 							}
 						}
-					}
-					else if(obj == null)
-					{	
+					} else if (obj == null) {
 						if (!Strings.isNullOrEmpty(path)) {
 							dataService.remove(path);
 						}
-					}
-					else
-					{
+					} else {
 						if (!Strings.isNullOrEmpty(path)) {
 							dataService.updateSimpleValue(path, obj);
 						}
 					}
-				}
-				else 
-				{
+				} else {
 					if (!Strings.isNullOrEmpty(path)) {
 						dataService.remove(path);
 					}
 				}
-				
+
 			}
 		} catch (Exception e) {
 			throw new RuntimeException(msg.toString(), e);
 		}
-	}
-
-	private String extractPath(JSONObject message) throws JSONException {
-		int servletPathLength = servletPath.length();
-		int repositoryNameLength = repositoryName.length();
-		String path = (String) message.get("path");
-		int indexOfServletPath = path.indexOf(servletPath);
-		if(indexOfServletPath > -1)
-		{
-			int substringIndex = indexOfServletPath + servletPathLength + repositoryNameLength + 1;
-			return path.substring(substringIndex);
-		}
-		else
-		{
-			return path;
-		}
-	}
-
-	private void broadcast(String message) throws IOException {
-		for (RoadrunnerMessageInbound connection : connections) {
-			CharBuffer buffer = CharBuffer.wrap(message);
-			connection.getWsOutbound().writeTextMessage(buffer);
-		}
-	}
-
-	public void child_added(String name, String path, String parent,
-			JSONObject node, String prevChildName, boolean hasChildren,
-			long numChildren) {
-		try {
-			JSONObject broadcast = new JSONObject();
-			broadcast.put("type", "child_added");
-			broadcast.put("name", name);
-			broadcast.put("path", path);
-			broadcast.put("parent", parent);
-			broadcast.put("payload", node);
-			broadcast.put("prevChildName", prevChildName);
-			broadcast.put("hasChildren", hasChildren);
-			broadcast.put("numChildren", numChildren);
-			if(listenerAttached(path))
-			{
-				getWsOutbound().writeTextMessage(CharBuffer.wrap(broadcast.toString()));
-			}
-		} catch (Exception exp) {
-			throw new RuntimeException(exp);
-		}
-	}
-
-	public void child_removed(String path, JSONObject payload) {
-		try {
-			JSONObject broadcast = new JSONObject();
-			broadcast.put("type", "child_removed");
-			broadcast.put("path", path);
-			broadcast.put("payload", payload);
-			getWsOutbound().writeTextMessage(
-					CharBuffer.wrap(broadcast.toString()));
-		} catch (Exception exp) {
-			throw new RuntimeException(exp);
-		}
-	}
-
-	public void child_changed(String name, String path, String parent,
-			JSONObject node, String prevChildName, boolean hasChildren,
-			long numChildren) {
-		try {
-			JSONObject broadcast = new JSONObject();
-			broadcast.put("type", "child_changed");
-			broadcast.put("name", name);
-			broadcast.put("path", path);
-			broadcast.put("parent", parent);
-			broadcast.put("payload", node);
-			broadcast.put("prevChildName", prevChildName);
-			broadcast.put("hasChildren", hasChildren);
-			broadcast.put("numChildren", numChildren);
-			getWsOutbound().writeTextMessage(
-					CharBuffer.wrap(broadcast.toString()));
-		} catch (Exception exp) {
-			throw new RuntimeException(exp);
-		}
-	}
-
-	public void child_moved(JSONObject childSnapshot, String prevChildName,
-			boolean hasChildren, long numChildren) {
-		try {
-			JSONObject broadcast = new JSONObject();
-			broadcast.put("type", "child_moved");
-			broadcast.put("payload", childSnapshot);
-			broadcast.put("prevChildName", prevChildName);
-			broadcast.put("hasChildren", hasChildren);
-			broadcast.put("numChildren", numChildren);
-			getWsOutbound().writeTextMessage(
-					CharBuffer.wrap(broadcast.toString()));
-		} catch (Exception exp) {
-			throw new RuntimeException(exp);
-		}
-	}
-
-	@Override
-	protected void onClose(int status) {
-		super.onClose(status);
-		try {
-			dataService.shutdown();
-		} catch (Exception exp) {
-			throw new RuntimeException(exp);
-		}
-	}
-
-	private boolean listenerAttached(String path) {
-		for(String listenerPath : attached_listeners)
-		{
-			if(path.startsWith(listenerPath))
-			{
-				return true;
-			}
-		}
-		return false;
 	}
 }
--- roadrunner-tomcat/src/main/java/org/roadrunner/server/RoadrunnerWebSocketServlet.java
+++ roadrunner-tomcat/src/main/java/org/roadrunner/server/RoadrunnerWebSocketServlet.java
@@ -1,45 +1,84 @@
 package org.roadrunner.server;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.websocket.StreamInbound;
 import org.apache.catalina.websocket.WebSocketServlet;
 import org.roadrunner.modeshape.ModeShapeServiceFactory;
 
+import com.google.common.io.Resources;
+
 /**
  * Servlet implementation class ChatServlet
  */
-@WebServlet(value = "/rr/*", loadOnStartup = 1)
-public class RoadrunnerWebSocketServlet extends WebSocketServlet
-{
-  private static final long serialVersionUID = 1L;
-  
-  private ModeShapeServiceFactory dataServiceFactory = new ModeShapeServiceFactory();
+public class RoadrunnerWebSocketServlet extends WebSocketServlet {
+	private static final long serialVersionUID = 1L;
 
-  @Override
-  protected StreamInbound createWebSocketInbound(String subProtocol, HttpServletRequest request)
-  {
-    String contextPath = request.getContextPath();
-    String servletPath = request.getServletPath();
-    String requestURI = request.getRequestURI();
-    String collectionPath = requestURI.substring((contextPath + servletPath).length() + 1);
-    try
-    {
-      return new RoadrunnerMessageInbound(contextPath + servletPath, collectionPath, dataServiceFactory,dataServiceFactory);
-    }
-    catch (Exception e)
-    {
-      throw new RuntimeException(e);
-    }
-  }
+	private ModeShapeServiceFactory dataServiceFactory = new ModeShapeServiceFactory();
 
-  @Override
-  public void init(ServletConfig config) throws ServletException
-  {
-    super.init(config);
-  }
+	@Override
+	protected StreamInbound createWebSocketInbound(String subProtocol,
+			HttpServletRequest request) {
+		String contextPath = request.getContextPath();
+		String servletPath = request.getServletPath();
+		String requestURI = request.getRequestURI();
+		String collectionPath = requestURI
+				.substring((contextPath + servletPath).length() + 1);
+		try {
+			return new RoadrunnerMessageInbound(contextPath + servletPath,
+					collectionPath, dataServiceFactory, dataServiceFactory);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		if (headerContainsToken(req, "upgrade", "websocket")) {
+			super.doGet(req, resp);
+		} else {
+			URL url = RoadrunnerWebSocketServlet.class.getClassLoader()
+					.getResource("roadrunner.js");
+			resp.setContentType("application/javascript");
+			resp.setCharacterEncoding("UTF-8");
+
+			ServletOutputStream outputStream = resp.getOutputStream();
+			Resources.copy(url, outputStream);
+			outputStream.close();
+		}
+	}
+
+	/*
+	 * This only works for tokens. Quoted strings need more sophisticated
+	 * parsing.
+	 */
+	private boolean headerContainsToken(HttpServletRequest req,
+			String headerName, String target) {
+		Enumeration<String> headers = req.getHeaders(headerName);
+		while (headers.hasMoreElements()) {
+			String header = headers.nextElement();
+			String[] tokens = header.split(",");
+			for (String token : tokens) {
+				if (target.equalsIgnoreCase(token.trim())) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
 
 }
