6428e426a6f0867edf4997460460e8472880a377
zerooneinfinity <christophgrotz@gmail.com>
2013-07-07 22:28:42 +0200

    Refactoring

--- roadrunner-coyote/src/main/java/de/skiptag/roadrunner/coyote/actions/SetAction.java
+++ roadrunner-coyote/src/main/java/de/skiptag/roadrunner/coyote/actions/SetAction.java
@@ -9,42 +9,42 @@ import de.skiptag.roadrunner.core.dataService.DataService;
 
 public class SetAction {
 
-	private DataService dataService;
+    private DataService dataService;
 
-	public SetAction(DataService dataService) {
-		this.dataService = dataService;
-	}
+    public SetAction(DataService dataService) {
+	this.dataService = dataService;
+    }
 
-	public void handle(JSONObject message, String path) throws JSONException {
-		JSONObject payload;
-		if (message.has("payload")) {
-			Object obj = message.get("payload");
-			if (obj instanceof JSONObject) {
-				payload = (JSONObject) obj;
-				if (payload instanceof JSONObject) {
-					if (Strings.isNullOrEmpty(path)) {
-						dataService.update(null, payload);
-					} else {
-						dataService.update(path, payload);
-					}
-				}
-			} else if (obj == null || obj == JSONObject.NULL) {
-				if (!Strings.isNullOrEmpty(path)) {
-					message.put("oldValue", dataService.get(path));
-					dataService.remove(path);
-				}
-			} else {
-				if (!Strings.isNullOrEmpty(path)) {
-					dataService.updateSimpleValue(path, obj);
-				}
-			}
-		} else {
-			if (!Strings.isNullOrEmpty(path)) {
-				message.put("oldValue", dataService.get(path));
-				dataService.remove(path);
-			}
+    public void handle(JSONObject message, String path) throws JSONException {
+	JSONObject payload;
+	if (message.has("payload")) {
+	    Object obj = message.get("payload");
+	    if (obj instanceof JSONObject) {
+		payload = (JSONObject) obj;
+		if (payload instanceof JSONObject) {
+		    if (Strings.isNullOrEmpty(path)) {
+			dataService.update(null, payload);
+		    } else {
+			dataService.update(path, payload);
+		    }
 		}
-
+	    } else if (obj == null || obj == JSONObject.NULL) {
+		if (!Strings.isNullOrEmpty(path)) {
+		    message.put("oldValue", dataService.get(path));
+		    dataService.remove(path);
+		}
+	    } else {
+		if (!Strings.isNullOrEmpty(path)) {
+		    dataService.update(path, obj);
+		}
+	    }
+	} else {
+	    if (!Strings.isNullOrEmpty(path)) {
+		message.put("oldValue", dataService.get(path));
+		dataService.remove(path);
+	    }
 	}
+
+    }
 
 }
