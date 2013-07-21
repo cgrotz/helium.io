8f21e76396c33449e295450a79fc5c98017abbb1
zerooneinfinity <christophgrotz@gmail.com>
2013-07-22 00:32:32 +0200

    Reenabled Functionality for Servlet

--- roadrunner-servlet/src/main/java/de/skiptag/roadrunner/server/RoadrunnerWebSocketServlet.java
+++ roadrunner-servlet/src/main/java/de/skiptag/roadrunner/server/RoadrunnerWebSocketServlet.java
@@ -3,6 +3,8 @@ package de.skiptag.roadrunner.server;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Enumeration;
 
 import javax.servlet.ServletConfig;
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.websocket.StreamInbound;
 import org.apache.catalina.websocket.WebSocketServlet;
+import org.apache.tomcat.util.http.fileupload.util.Streams;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Charsets;
@@ -32,6 +35,8 @@ public class RoadrunnerWebSocketServlet extends WebSocketServlet {
     private static final long serialVersionUID = 1L;
     private static final org.slf4j.Logger logger = LoggerFactory.getLogger(RoadrunnerWebSocketServlet.class.getName());
     private Roadrunner roadrunner;
+    private String journalDirectory;
+    private String snapshotDirectory;
 
     @Override
     protected StreamInbound createWebSocketInbound(String subProtocol,
@@ -64,34 +69,60 @@ public class RoadrunnerWebSocketServlet extends WebSocketServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
 	super.init(config);
-	String journalDirectory = Preconditions.checkNotNull(config.getInitParameter("journalDirectory"), "JournalDirectory must be configured in web.xml");
-	String snapshotDirectory = Preconditions.checkNotNull(config.getInitParameter("snapshotDirectory"), "SnapshotDirectory must be configured in web.xml");
-	String basePath = Preconditions.checkNotNull(config.getInitParameter("basePath"), "BasePath must be configured in web.xml");
-	try {
-	    Optional<File> snapshotDir = Optional.of(new File(snapshotDirectory));
-	    this.roadrunner = new Roadrunner(basePath, journalDirectory,
-		    snapshotDir);
-	} catch (IOException e) {
-	    logger.error("Error loading Roadrunner", e);
-	}
+	journalDirectory = Preconditions.checkNotNull(config.getInitParameter("journalDirectory"), "JournalDirectory must be configured in web.xml");
+	snapshotDirectory = Preconditions.checkNotNull(config.getInitParameter("snapshotDirectory"), "SnapshotDirectory must be configured in web.xml");
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp)
 	    throws ServletException, IOException {
-	if (headerContainsToken(req, "upgrade", "websocket")) {
-	    super.service(req, resp);
+	if (roadrunner == null) {
+	    String basePath = req.getScheme() + "://" + req.getServerName()
+		    + ":" + req.getServerPort() + req.getContextPath() + ""
+		    + req.getServletPath();
+	    try {
+		Optional<File> snapshotDir = Optional.of(new File(
+			snapshotDirectory));
+		this.roadrunner = new Roadrunner(basePath, journalDirectory,
+			snapshotDir);
+	    } catch (IOException e) {
+		logger.error("Error loading Roadrunner", e);
+	    }
+	}
+
+	if (resourceForURI(req) != null) {
+	    ServletOutputStream outputStream = resp.getOutputStream();
+	    Streams.copy(resourceForURI(req).openStream(), outputStream, true);
 	} else if (req.getMethod().equals("GET")
-		&& req.getRequestURI().equals("roadrunner.js")) {
+		&& resourceForURI(req, "/roadrunner.js")) {
 	    resp.setContentType("application/javascript");
 	    resp.setCharacterEncoding("UTF-8");
 
 	    ServletOutputStream outputStream = resp.getOutputStream();
 	    outputStream.write(roadrunner.loadJsFile().getBytes());
 	    outputStream.close();
+	} else if (headerContainsToken(req, "upgrade", "websocket")) {
+	    super.service(req, resp);
 	} else {
 	    handleRestCall(req, resp);
 	}
+    }
+
+    private URL resourceForURI(HttpServletRequest req)
+	    throws MalformedURLException {
+	String requestURI = req.getRequestURI();
+	String contextPath = req.getContextPath();
+	String resourcePath = requestURI.replaceAll(contextPath, "");
+	URL url = req.getServletContext().getResource(resourcePath);
+	return url;
+    }
+
+    private boolean resourceForURI(HttpServletRequest req, String uri)
+	    throws MalformedURLException {
+	String requestURI = req.getRequestURI();
+	String contextPath = req.getContextPath();
+	String resourcePath = requestURI.replaceAll(contextPath, "");
+	return uri.equals(resourcePath);
     }
 
     private void handleRestCall(HttpServletRequest req, HttpServletResponse resp)
@@ -112,9 +143,4 @@ public class RoadrunnerWebSocketServlet extends WebSocketServlet {
 	}
 	resp.setContentType("application/json; charset=UTF-8");
     }
-
-    private String getHttpSocketLocation(HttpServletRequest req) {
-	return "";
-    }
-
 }
